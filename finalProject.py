19671aa9bc2cbe86aa7d681b6df95a3813bbebe4
Matt Ellis <melliss67@yahoo.com>
2015-05-15 20:27:03 -0700

    restaurant editing/deleting mock up added

--- finalProject.py
+++ finalProject.py
@@ -1,22 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+#restaurants = []
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
+
 @app.route('/')
-@app.route('/restaurant')
+@app.route('/restaurants')
 def showRestaurants():
-	return 'This page will show all my restaurants'
+	return render_template('restaurants.html', restaurants=restaurants)
 	
 @app.route('/restaurant/new')
 def newRestaurant():
-	return 'This page will be for making a new restaurant'
+	return render_template('newrestaurant.html')
 
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-	return 'This page will be for editing restaurant %s' % restaurant_id
+	return render_template('editrestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-	return 'This page will be for deleting restaurant %s' % restaurant_id
+	return render_template('deleterestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
