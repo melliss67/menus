69e42a3d7d72cec1dec59aadcb70c5d068383b52
Matt Ellis <melliss67@yahoo.com>
2015-05-20 20:15:46 -0700

    starting crud methods

--- finalProject.py
+++ finalProject.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 #Fake Restaurants
 restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
 
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+#restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
 #restaurants = []
 
 #Fake Menu Items
@@ -13,13 +23,21 @@ item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$
 
 
 @app.route('/')
-@app.route('/restaurants')
+@app.route('/restaurants', methods=['GET', 'POST'])
 def showRestaurants():
+	restaurants = session.query(Restaurant).all()
 	return render_template('restaurants.html', restaurants=restaurants)
 	
-@app.route('/restaurant/new')
+@app.route('/restaurant/new', methods=['GET', 'POST'])
 def newRestaurant():
-	return render_template('newrestaurant.html')
+	if request.method == 'POST':
+		newRestaurant = Restaurant(name = request.form['name'])
+		session.add(newRestaurant)
+		session.commit()
+		restaurants = session.query(Restaurant).all()
+		return render_template('restaurants.html', restaurants=restaurants)
+	else:
+		return render_template('newrestaurant.html')
 
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
